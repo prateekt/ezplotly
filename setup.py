905a7887edb35362ce6cd0290fdfc29f0a1c8308
Prateek Tandon <prateek.tandon@roswellbiotech.com>
2020-07-15 14:31:51 -0700

    pip package

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="EasyPlotly",
+    version="0.0.1",
+    author="Prateek Tandon",
+    author_email="prateek1.tandon@gmail.com",
+    description="An easy wrapper for making plotly plots in python jupyter notebooks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/prateekt/EasyPlotly",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
